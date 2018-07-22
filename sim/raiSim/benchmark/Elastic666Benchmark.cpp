670a96ff0e183b25b29d09442e5873dd3863d430
donghokang <east0822@gmail.com>
2018-07-22 16:35:04 +0200

    - elastic 666 image

--- sim/raiSim/benchmark/Elastic666Benchmark.cpp
+++ sim/raiSim/benchmark/Elastic666Benchmark.cpp
@@ -51,7 +51,9 @@ void setupWorld() {
   rai::RandomNumberGenerator<double> rand;
   rand.seed(benchmark::elasticsixsixsix::params.randomSeed);
 
-  auto checkerboard = sim->addCheckerboard(5.0, 500.0, 500.0, 0.1, -1, rai_sim::GRID);
+  auto checkerboard = sim->addCheckerboard(5.0, 500.0, 500.0, 0.1, -1);
+  std::vector<float> spec = {0.0, 0.0, 0.0}, amb = {5.0, 5.0, 5.0}, diff = {0.0,0.0,0.0};
+  checkerboard.visual()[0]->setLightProp(amb, diff, spec, 0.2);
   checkerboard->setMaterial(sim->getMaterialKey("ground"));
 
   for(int i = 0; i < benchmark::elasticsixsixsix::params.n; i++) {
@@ -80,19 +82,19 @@ void setupWorld() {
 
         if(benchmark::elasticsixsixsix::options.gui) {
           if((i + j + k) % 3 == 0) {
-            obj.visual()[0]->setColor({0.5373,
-                                       0.6471,
-                                       0.3059});
+            obj.visual()[0]->setColor({1,
+                                       0,
+                                       0});
           }
           else if((i + j + k) % 3 == 1) {
-            obj.visual()[0]->setColor({0.5373,
-                                       0.6471,
-                                       0.3059});
+            obj.visual()[0]->setColor({0,
+                                       1,
+                                       0});
           }
           else if((i + j + k) % 3 == 2) {
-            obj.visual()[0]->setColor({0.5373,
-                                       0.6471,
-                                       0.3059});
+            obj.visual()[0]->setColor({0,
+                                       0,
+                                       1});
           }
         }
 
@@ -105,8 +107,7 @@ void setupWorld() {
     sim->setLightPosition((float)benchmark::elasticsixsixsix::params.lightPosition[0],
                           (float)benchmark::elasticsixsixsix::params.lightPosition[1],
                           (float)benchmark::elasticsixsixsix::params.lightPosition[2]);
-    sim->cameraFollowObject(objList[objList.size() / 2], {0, 5, 2});
-  }
+    sim->cameraFollowObject(objList[objList.size() / 2], {0, 5, 1});  }
 }
 
 double simulationLoop(bool timer = true, bool error = true) {
